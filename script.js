560c8e6adbf8f52450854325a6c81bd7e3ea7068
wkarol <122057880+karolgalindo02@users.noreply.github.com>
2023-07-11 22:27:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
-const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const poke_container = document.getElementById("poke-container");
+const pokemon_count = 150;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
-	electric: '#FCF7DE',
-	water: '#DEF3FD',
-	ground: '#f4e7da',
-	rock: '#d5d5d4',
-	fairy: '#fceaff',
-	poison: '#98d7a5',
-	bug: '#f8d5a3',
-	dragon: '#97b3e6',
-	psychic: '#eaeda1',
-	flying: '#F5F5F5',
-	fighting: '#E6E0D4',
-	normal: '#F5F5F5'
-}
+    electric: '#FCF7DE',
+    water: '#DEF3FD',
+    ground: '#f4e7da',
+    rock: '#d5d5d4',
+    fairy: '#fceaff',
+    poison: '#98d7a5',
+    bug: '#f8d5a3',
+    dragon: '#97b3e6',
+    psychic: '#eaeda1',
+    flying: '#F5F5F5',
+    fighting: '#E6E0D4',
+    normal: '#F5F5F5'
+};
+const main_types = Object.keys(colors);
 
-const main_types = Object.keys(colors)
-
-const fetchPokemons = async () => {
+const fetchPokemons = async (pokemon_count) => {
     for(let i = 1; i <= pokemon_count; i++) {
         await getPokemon(i)
     }
@@ -27,13 +26,37 @@ const fetchPokemons = async () => {
 
 const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    const res = await fetch(url)
-    const data = await res.json()
-    createPokemonCard(data)
+    const res = await fetch(url);
+    const data = await res.json();
+    createPokemonCard(data);
 }
+
+
 
 const createPokemonCard = (pokemon) => {
-    
+    const card = document.createElement("div");
+    card.classList.add("pokemon");
+
+    const container = document.createElement("div");
+    container.classList.add("img-container");
+
+    const sprite = document.createElement("img");
+    sprite.src = pokemon.sprites.front_default;
+
+    container.appendChild(sprite);
+
+    const number = document.createElement("p");
+    number.textContent = `#${pokemon.id.toString().padStart(3, "0")}`;
+
+    const name = document.createElement("p");
+    name.classList.add("name");
+    name.textContent = pokemon.name;
+
+    card.appendChild(container);
+    card.appendChild(number);
+    card.appendChild(name);
+
+    poke_container.appendChild(card);
 }
 
-fetchPokemons()
+fetchPokemons(pokemon_count);
