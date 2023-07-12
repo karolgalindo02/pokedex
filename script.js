737429c22e9938aca5d8ebeb7427c29613a752d5
tellmejjulian <133469855+tellmejjulian@users.noreply.github.com>
2023-07-12 18:30:54 -0500

    color cards.js

--- script.js
+++ script.js
@@ -20,43 +20,54 @@ const main_types = Object.keys(colors);
 
 const fetchPokemons = async (pokemon_count) => {
     for(let i = 1; i <= pokemon_count; i++) {
-        await getPokemon(i)
+        await getPokemon(i); 
     }
 }
 
 const getPokemon = async (id) => {
-    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
-    const res = await fetch(url);
-    const data = await res.json();
-    createPokemonCard(data);
+    const url = https://pokeapi.co/api/v2/pokemon/${id};  
+    const res = await fetch(url); 
+    const data = await res.json(); 
+    createPokemonCard(data); 
 }
 
-
-
 const createPokemonCard = (pokemon) => {
-    const card = document.createElement("div");
-    card.classList.add("pokemon");
+    const card = document.createElement("div"); 
+    card.classList.add("pokemon"); 
 
-    const container = document.createElement("div");
-    container.classList.add("img-container");
+    const container = document.createElement("div"); 
+    container.classList.add("img-container"); 
 
-    const sprite = document.createElement("img");
-    sprite.src = pokemon.sprites.front_default;
+    const sprite = document.createElement("img"); 
+    sprite.src = pokemon.sprites.front_default; 
 
-    container.appendChild(sprite);
+    container.appendChild(sprite); 
 
     const number = document.createElement("p");
-    number.textContent = `#${pokemon.id.toString().padStart(3, "0")}`;
+    number.classList.add("number"); 
+    number.textContent = `#${pokemon.id.toString().padStart(3, "0")}`; 
 
-    const name = document.createElement("p");
-    name.classList.add("name");
-    name.textContent = pokemon.name;
+    const name = document.createElement("h3");
+    name.classList.add("name"); 
+    name.textContent = capitalizeFirstLetter(pokemon.name); 
 
-    card.appendChild(container);
-    card.appendChild(number);
-    card.appendChild(name);
+    const type = document.createElement("p");
+    type.classList.add("info"); 
+    type.textContent = Type: `${pokemon.types[0].type.name}`;
 
-    poke_container.appendChild(card);
+    const cardColor = colors[pokemon.types[0].type.name]; 
+    card.style.backgroundColor = cardColor; 
+  
+    card.appendChild(container); 
+    card.appendChild(number); 
+    card.appendChild(name); 
+    card.appendChild(type); 
+
+    poke_container.appendChild(card); 
+}
+
+const capitalizeFirstLetter = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
 fetchPokemons(pokemon_count);
